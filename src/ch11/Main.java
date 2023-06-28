5099b7f2edcdcc57975e4249b026534682532c4c
LeeHyunMee <hbin1028@gmail.com>
2023-06-28 16:32:09 +0900

    term 값 수정

--- src/ch11/Main.java
+++ src/ch11/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            int term = 10;
+            int term = 50;
             int amount = 240;
             int result = amount / term;
 
