415f4cf44967fc95deae2496a64cc9b72776c465
LeeHyunMee <hbin1028@gmail.com>
2023-06-28 16:57:57 +0900

    comment add

--- src/ch12/Main.java
+++ src/ch12/Main.java
@@ -5,7 +5,7 @@ public class Main {
         MyService srv1 = new MyService("수강 관리 서비스");
         MyService srv2  = new MyService("----------> 학생 정보 관리 서비스");
 
-        srv1.start();
+        srv1.start();   // 스레드 시작
         srv2.start();
     }
 }
